fb41755da7af7c12a4545cc29453a5b6009c9e36
DengJingChao <harden6@163.com>
2023-04-07 01:40:02 +0800

    label的折叠展开

---
+++ ChatAiDemo/ChatAiDemo/NSMutableAttributedString+Extension.swift
@@ -0,0 +1,97 @@
+//
+//  NSMutableAttributedString+Extension.swift
+//  CL
+//
+//  Created by Chen JmoVxia on 2021/3/26.
+//  Copyright © 2021 JmoVxia. All rights reserved.
+//
+
+import UIKit
+
+extension NSMutableAttributedString {
+    /// 添加字符串并为此段添加对应的Attribute
+    @discardableResult
+    func addText(_ text: String, attributes: ((_ item: AttributesItem) -> Void)? = nil) -> NSMutableAttributedString {
+        let item = AttributesItem()
+        attributes?(item)
+        append(NSMutableAttributedString(string: text, attributes: item.attributes))
+        return self
+    }
+
+    /// 添加Attribute作用于当前字符串
+    @discardableResult
+    func addAttributes(_ attributes: (_ item: AttributesItem) -> Void, rang: NSRange? = nil, replace: Bool = false) -> NSMutableAttributedString {
+        let item = AttributesItem()
+        attributes(item)
+        enumerateAttributes(in: rang ?? NSRange(string.startIndex ..< string.endIndex, in: string), options: .reverse) { oldAttribute, oldRange, _ in
+            var newAtt = oldAttribute
+            for attribute in item.attributes where replace ? true : !oldAttribute.keys.contains(attribute.key) {
+                newAtt[attribute.key] = attribute.value
+            }
+            addAttributes(newAtt, range: oldRange)
+        }
+        return self
+    }
+
+    /// 添加图片
+    @discardableResult
+    func addImage(_ image: UIImage?, _ bounds: CGRect) -> NSMutableAttributedString {
+        guard let image = image else { return self }
+        let attch = NSTextAttachment()
+        attch.image = image
+        attch.bounds = bounds
+        append(NSAttributedString(attachment: attch))
+        return self
+    }
+}
+
+//extension NSMutableAttributedString {
+//    /// 正则替换网页
+//    /// - Parameter attributes: 替换的属性
+//    /// - Returns: 替换结果，新的富文本
+//    func replaceLink(_ attributes: (_ item: AttributesItem) -> Void) -> (matchs: [Regex.Match], attributedString: NSMutableAttributedString) {
+//        var linkRanges = [Regex.Match]()
+//        let linkRegex = try! Regex("(([hH][tT]{2}[pP]://|[hH][tT]{2}[pP][sS]://|[wW][aA][pP].|[fF][tT][pP].|[fF][iI][lL][eE].)[-A-Za-z0-9+&@#/%?=~_|!:,.;']+[-A-Za-z0-9+&@#/%=~_|])|(https?:/{2})?((?!0)[0-9]{1,3}(\\.([0-9]{1,3})){3})|((https?:/{2})?(([-a-zA-Z_+~#=]{1,256}\\.)?(([a-zA-Z0-9]{1,63})\\.){1,3}([a-zA-Z]{1,8}))(:[0-9]{2,5})?([/?]([-a-zA-Z0-9@:%_+.~#?&/=]*))?)")
+//        linkRanges = linkRegex.matches(in: string)
+//        for match in linkRanges {
+//            addAttributes(attributes, rang: match.result.range, replace: true)
+//        }
+//        return (linkRanges, self)
+//    }
+//
+//    /// 正则替换电话号码
+//    /// - Parameter attributes: 替换的属性
+//    /// - Returns: 替换结果，新的富文本
+//    func replacePhone(_ attributes: (_ item: AttributesItem) -> Void) -> (matchs: [Regex.Match], attributedString: NSMutableAttributedString) {
+//        var phoneRanges = [Regex.Match]()
+//        let phoneRegex = try! Regex("([+]?[(（]?[+]?(?<!\\d)(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)[)）—-]?\\d{5,14}(?!\\d))|((?<!\\d)([2-8]\\d{6,7})(?!\\d)|((?<!\\d)[(（]?010[)）—-]?\\d{7,8}(?!\\d))|((?<!\\d)[(（]?0[2-9]\\d{1,2}[)）—-]?\\d{7,8}(?!\\d)))")
+//        phoneRanges = phoneRegex.matches(in: string)
+//        for match in phoneRanges {
+//            addAttributes(attributes, rang: match.result.range, replace: true)
+//        }
+//        return (phoneRanges, self)
+//    }
+//
+//    /// 正则富文本链接和电话号码
+//    func replace(_ linkAttributes: (_ item: AttributesItem) -> Void, phoneAttributes: (_ item: AttributesItem) -> Void) -> NSMutableAttributedString {
+//        let linkPattern = """
+//        (((ht|f)tps?):\\/\\/)?[\\w-]+(\\.[\\w-]+)+([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?
+//        """
+//        let phonePattern = """
+//        ((?:\\+?86)?1(?:3\\d{3}|5[^4\\D]\\d{2}|8\\d{3}|7(?:[235-8]\\d{2}|4(?:0\\d|1[0-2]|9\\d))|9[0-35-9]\\d{2}|66\\d{2})\\d{6})|([+]?[(（]?[+]?(?<!\\d)(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)[)）—-]?\\d{5,14}(?!\\d))|((?<!\\d)([2-8]\\d{6,7})(?!\\d)|((?<!\\d)[(（]?010[)）—-]?\\d{7,8}(?!\\d))|((?<!\\d)[(（]?0[2-9]\\d{1,2}[)）—-]?\\d{7,8}(?!\\d)))
+//        """
+//        let regex = try! Regex("\(linkPattern)|\(phonePattern)")
+//        for match in regex.matches(in: string) {
+//            if match.string.matches(linkPattern),
+//               let url = match.string.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+//            {
+//                addAttributes(linkAttributes, rang: match.result.range, replace: true)
+//                addAttributes([.link: "Link://\(url)"], range: match.result.range)
+//            } else if match.string.matches(phonePattern) {
+//                addAttributes(phoneAttributes, rang: match.result.range, replace: true)
+//                addAttributes([.link: "Phone://\(match.string)"], range: match.result.range)
+//            }
+//        }
+//        return self
+//    }
+//}
--- ChatAiDemo/ChatAiDemo/ViewController.swift
+++ ChatAiDemo/ChatAiDemo/ViewController.swift
@@ -17,72 +17,133 @@ class ViewController: UIViewController {
         
         self.navigationItem.title = "AI Box"
         
-        let textFeild = UITextField(frame: CGRect(x: 20, y: 120, width: 200, height: 30))
-        textFeild.borderStyle = .roundedRect
-        textFeild.textColor = .red
-        textFeild.textAlignment = .left
-        textFeild.placeholder = "请输入姓名"
-        self.view.addSubview(textFeild)
+//        let textFeild = UITextField(frame: CGRect(x: 20, y: 120, width: 200, height: 30))
+//        textFeild.borderStyle = .roundedRect
+//        textFeild.textColor = .red
+//        textFeild.textAlignment = .left
+//        textFeild.placeholder = "请输入姓名"
+//        self.view.addSubview(textFeild)
+//
+//        let btn = UIButton()
+//        btn.addTarget(self, action: #selector(buttonDidClick), for: .touchUpInside)
+//        btn.backgroundColor = .red
+//        self.view.addSubview(btn)
+//        btn.snp.makeConstraints { make in
+//            make.centerX.centerY.equalToSuperview()
+//            make.size.equalTo(CGSize(width: 100, height: 100))
+//        }
+//
+//        let model1 = CAChatModel.createModel("1", "harden", 1)
+//        let model2 = CAChatModel.createModel("harden", "1", 2)
+//        let model3 = CAChatModel.createModel("1", "harden", 3)
+//        let model4 = CAChatModel.createModel("harden", "1", 4)
+//        let model5 = CAChatModel.createModel("1", "harden", 5)
+//        let model6 = CAChatModel.createModel("harden", "1", 6)
+//        let model7 = CAChatModel.createModel("1", "harden", 7)
+//        let model8 = CAChatModel.createModel("harden", "1", 8)
+//        let model9 = CAChatModel.createModel("1", "harden", 9)
+//        let model10 = CAChatModel.createModel("harden", "1", 10)
+//
+//        let arr: [CAChatModel] = [model1, model2, model3, model4, model5, model6, model7, model8, model9, model10]
+//
+//        print("1")
+//        let sendArr: [CAChatModel] = arr.filter { $0.send_id == "1" }
+//        let repleArr: [CAChatModel] = arr.filter { $0.repley_id == "1" }
+//        let tempArr = sendArr.flatMap { send in
+//            repleArr.map { reply in
+//                compareModel(send, reply)
+//            }
+//        }
+//        let resultArr1 = tempArr.filter { $0?.count ?? 0 > 0 }
+//        let resultArr2 = tempArr.filter { !($0?.isEmpty ?? true) }
+//        print("2")
+//
+//        var dasdkArr: [Any] = []
+//        for (index, element) in arr.enumerated() {
+//            if (index > 0) {
+//                let lastElement = arr[index - 1]
+//                if (lastElement.send_id == "1" && element.repley_id == "1") {
+//                    dasdkArr.append([lastElement, element])
+//                }
+//            }
+//            print(index,element)
+//        }
+//        print("3")
+//
+//
+//
+//
+//
+//
+//        self.view.addSubview(self.label1)
+//        self.label1.frame = CGRect(x: 100, y: 150, width: 200, height: 50)
+//
+//
         
-        let btn = UIButton()
-        btn.addTarget(self, action: #selector(buttonDidClick), for: .touchUpInside)
-        btn.backgroundColor = .red
-        self.view.addSubview(btn)
-        btn.snp.makeConstraints { make in
-            make.centerX.centerY.equalToSuperview()
-            make.size.equalTo(CGSize(width: 100, height: 100))
-        }
         
-        let model1 = CAChatModel.createModel("1", "harden", 1)
-        let model2 = CAChatModel.createModel("harden", "1", 2)
-        let model3 = CAChatModel.createModel("1", "harden", 3)
-        let model4 = CAChatModel.createModel("harden", "1", 4)
-        let model5 = CAChatModel.createModel("1", "harden", 5)
-        let model6 = CAChatModel.createModel("harden", "1", 6)
-        let model7 = CAChatModel.createModel("1", "harden", 7)
-        let model8 = CAChatModel.createModel("harden", "1", 8)
-        let model9 = CAChatModel.createModel("1", "harden", 9)
-        let model10 = CAChatModel.createModel("harden", "1", 10)
-        
-        let arr: [CAChatModel] = [model1, model2, model3, model4, model5, model6, model7, model8, model9, model10]
-        
-        print("1")
-        let sendArr: [CAChatModel] = arr.filter { $0.send_id == "1" }
-        let repleArr: [CAChatModel] = arr.filter { $0.repley_id == "1" }
-        let tempArr = sendArr.flatMap { send in
-            repleArr.map { reply in
-                compareModel(send, reply)
-            }
-        }
-        let resultArr1 = tempArr.filter { $0?.count ?? 0 > 0 }
-        let resultArr2 = tempArr.filter { !($0?.isEmpty ?? true) }
-        print("2")
-        
-        var dasdkArr: [Any] = []
-        for (index, element) in arr.enumerated() {
-            if (index > 0) {
-                let lastElement = arr[index - 1]
-                if (lastElement.send_id == "1" && element.repley_id == "1") {
-                    dasdkArr.append([lastElement, element])
-                }
-            }
-            print(index,element)
-        }
-        print("3")
-        
-        
-        
-        
-        
-        
-        self.view.addSubview(self.label1)
-        self.label1.frame = CGRect(x: 100, y: 150, width: 200, height: 50)
-        
-        
-        
-        
-        
+        initSubViews()
+        makeConstraints()
+        initData()
     }
+    
+    func initSubViews() {
+        view.addSubview(truncationLabel)
+    }
+    func makeConstraints() {
+        truncationLabel.snp.makeConstraints { make in
+            make.left.equalTo(15)
+            make.right.equalTo(-15)
+            make.top.equalTo(200)
+        }
+    }
+    func initData() {
+        truncationLabel.attributedText = NSMutableAttributedString("时间最会骗人，但也能让你明白，这世界上是没有什么不能失去的！离去的都是风景，留下的才是人生，走到最后的，才是对的人,记住，要的是重新开始，并不是不从零开始，我们要带着过去所经历的困难和那些所明白的道理，继续走下去。") { $0
+            .font(.systemFont(ofSize: 14))
+            .lineSpacing(5)
+            .foregroundColor("#5E6582".uiColor)
+            .lineBreakMode(.byWordWrapping)
+        }
+        truncationLabel.truncationToken = (NSAttributedString(" ... [展开]"){ $0
+            .foregroundColor("#1F70FF".uiColor)
+            .font(.systemFont(ofSize: 14))
+        }, NSAttributedString(" [折叠]"){ $0
+            .foregroundColor("#1F70FF".uiColor)
+            .font(.systemFont(ofSize: 14))
+        })
+        truncationLabel.reload()
+    }
+    
+    @objc func didClickLabelAction()  {
+        truncationLabel.isOpen.toggle()
+        truncationLabel.reload()
+    }
+    
+    private lazy var truncationLabel: CALabel = {
+        let view = CALabel()
+        view.numberOfLines = 3
+        view.contentEdgeInsets = .init(top: 10, left: 10, bottom: 10, right: 10)
+        view.layer.cornerRadius = 5
+        view.layer.borderColor = UIColor.orange.cgColor
+        view.layer.borderWidth = 0.5
+        view.addTarget(self, action: #selector(didClickLabelAction), for: .touchUpInside)
+        return view
+    }()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     lazy var label1: UILabel = UILabel.creatLabel("ddddd")
